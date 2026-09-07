18e995d9973579db73df7d479099dbf636b07289
stitchsorry7-hash <stitchsorry7@gmail.com>
2026-09-08 03:24:20 +0700

    Expand currency calculation test coverage

--- app/src/test/java/com/example/teraboxcurrency/CurrencyCalculatorTest.kt
+++ app/src/test/java/com/example/teraboxcurrency/CurrencyCalculatorTest.kt
@@ -10,7 +10,27 @@ class CurrencyCalculatorTest {
     }
 
     @Test
-    fun invalidInputReturnsZero() {
+    fun roundsToTwoDecimalPlaces() {
+        assertEquals("3.33", CurrencyCalculator.convert("1.00", "3.333"))
+    }
+
+    @Test
+    fun supportsDecimalAmounts() {
+        assertEquals("437.50", CurrencyCalculator.convert("12.5", "35"))
+    }
+
+    @Test
+    fun zeroAmountReturnsZero() {
+        assertEquals("0.00", CurrencyCalculator.convert("0", "35"))
+    }
+
+    @Test
+    fun invalidAmountReturnsZero() {
         assertEquals("0.00", CurrencyCalculator.convert("abc", "35"))
+    }
+
+    @Test
+    fun invalidRateReturnsZero() {
+        assertEquals("0.00", CurrencyCalculator.convert("100", "abc"))
     }
 }
